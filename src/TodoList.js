d350adb330274ae8a2d2ad33affa44200ca1979b
Torey Leggett <victoriahleggett@gmail.com>
2018-09-17 19:38:48 -0400

    first commit

---
+++ src/TodoList.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+
+
+class TodoList extends Component {
+  render() {
+    return (
+      <ul>
+          <li>Todo1</li>
+          <li>Todo2</li>
+          <li>Todo3</li>
+      </ul>
+    );
+  }
+}
+
+export default TodoList;
